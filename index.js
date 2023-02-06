fb1086646b808207c5268bf8962d517c9000412d
Gerónimo <gerolpz01@gmail.com>
2023-02-06 20:01:31 -0300

    Creados archivos base

---
+++ index.js
@@ -0,0 +1,11 @@
+localStorage.setItem("saludo", "hola mundo");
+console.log(localStorage.getItem("saludo"));
+
+
+function login(nombre, password){
+   //desencriptar password <-
+   let token = "asdsad12325455asdasdas";
+   
+   return token;
+
+}
